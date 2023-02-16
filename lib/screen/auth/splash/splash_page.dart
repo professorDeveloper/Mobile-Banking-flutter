938521371f2c59285ba39246edafd80df0c3d7b9
professorDeveloper <azamovhud007@outlook.com>
2023-02-16 15:39:46 +0500

    Add package

---
+++ lib/screen/auth/splash/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
